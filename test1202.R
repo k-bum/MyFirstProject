e543c92281788e2fc1a2893fd9937b4e0ef605f4
k-bum <kbum0209@gmail.com>
2021-12-31 06:31:33 +0900

    normal distribution

---
+++ test1202.R
@@ -0,0 +1,37 @@
+pnorm(0) #표준정규분포, 0.5
+pnorm(0, mean = 0, sd = 1)
+pnorm(3.27, mean = 0, sd = 1)
+pnorm(3.27)
+pnorm(64, mean = 71, sd = 4.5)
+1 - pnorm(64, mean = 71, sd = 4.5)
+1 - pnorm(1.56, mean = 0, sd = 1)
+pnorm(3.12) - pnorm(1.25)
+pnorm(3.12, mean = 0, sd = 1) - pnorm(1.25, mean = 0, sd = 1)
+
+pnorm(380, mean = 340, sd = 30)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
